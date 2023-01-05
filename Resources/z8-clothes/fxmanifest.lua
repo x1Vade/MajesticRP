5eca1de6a8b49962fe3df235c257fa2ef577de87
Vade <vadeontop@gmail.com>
2023-01-04 20:59:46 -0600

    Clothes

---
+++ Resources/z8-clothes/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+files {
+  "mp_f_freemode_01_np_shop.meta",
+  "mp_m_freemode_01_np_shop.meta",
+  "eup_ui.lua"
+}
+
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_f_freemode_01_np_shop.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "mp_m_freemode_01_np_shop.meta"
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_np_new.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_np_new.meta'
